9d8699e78e1b5993d061eb22f3daa701d17a0ab7
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-12-15 09:42:25 +0100

    Adds update-document (patch)

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 (async () => {
   require('dotenv').config()
+  const { writeFile } = require('fs').promises
   const getStudentData = require('./lib/get-tjommi-data')
+  const updateDocument = require('./lib/update-document')
   const logger = require('./lib/logger')
   const documents = require('./data/documents-copy.json')
+  let documentsDone = []
+  try {
+    const requiredDocumentsDone = require('./data/documents-done.json')
+    documentsDone = requiredDocumentsDone
+    logger('info', ['index', 'got', 'documents-done'])
+  } catch (error) {
+    logger('warn', ['index', 'no', 'documents-done', 'to be found'])
+  }
   logger('info', ['index', 'got', documents.length, 'documents left'])
   const document = documents.pop()
   const { _id, userId: teacher, studentUserName: student } = document
@@ -10,7 +20,16 @@
   logger('info', ['index', 'document', _id, 'student', student, 'teacher', teacher, 'lookup student', 'start'])
   const data = await getStudentData({ student, teacher })
   logger('info', ['index', 'document', _id, 'student', student, 'teacher', teacher, 'lookup student', 'success'])
-  console.log(JSON.stringify(data, null, 2))
-  const { mainGroupName } = data
-  logger('info', ['index', 'document', _id, 'student', student, 'teacher', teacher, 'mainGroupName', mainGroupName])
+  const { mainGroupName: studentMainGroupName } = data
+  logger('info', ['index', 'document', _id, 'student', student, 'teacher', teacher, 'studentMainGroupName', studentMainGroupName])
+  try {
+    const result = await updateDocument({ _id, studentMainGroupName })
+    logger('info', ['index', 'document', _id, 'student', student, 'teacher', teacher, 'studentMainGroupName', studentMainGroupName, 'result', result])
+    documentsDone.push({ document, studentMainGroupName: studentMainGroupName, success: true })
+    await writeFile('data/documents-done.json', JSON.stringify(documentsDone, null, 2), 'utf-8')
+    await writeFile('data/documents-copy.json', JSON.stringify(documents, null, 2), 'utf-8')
+  } catch (error) {
+    logger('error', ['index', 'document', _id, 'student', student, 'teacher', teacher, 'studentMainGroupName', studentMainGroupName, 'result', error])
+  }
+  process.exit()
 })()
