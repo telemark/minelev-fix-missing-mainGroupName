f6744446035270db8dde69468509ea1b31f0525f
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-12-15 10:40:15 +0100

    Adds while loop (patch)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 (async () => {
   require('dotenv').config()
   const { writeFile } = require('fs').promises
+  const sleep = require('then-sleep')
   const getStudentData = require('./lib/get-tjommi-data')
   const updateDocument = require('./lib/update-document')
   const logger = require('./lib/logger')
@@ -13,23 +14,28 @@
   } catch (error) {
     logger('warn', ['index', 'no', 'documents-done', 'to be found'])
   }
-  logger('info', ['index', 'got', documents.length, 'documents left'])
-  const document = documents.pop()
-  const { _id, userId: teacher, studentUserName: student } = document
-  logger('info', ['index', 'document', _id, 'start'])
-  logger('info', ['index', 'document', _id, 'student', student, 'teacher', teacher, 'lookup student', 'start'])
-  const data = await getStudentData({ student, teacher })
-  logger('info', ['index', 'document', _id, 'student', student, 'teacher', teacher, 'lookup student', 'success'])
-  const { mainGroupName: studentMainGroupName } = data
-  logger('info', ['index', 'document', _id, 'student', student, 'teacher', teacher, 'studentMainGroupName', studentMainGroupName])
-  try {
-    const result = await updateDocument({ _id, studentMainGroupName })
-    logger('info', ['index', 'document', _id, 'student', student, 'teacher', teacher, 'studentMainGroupName', studentMainGroupName, 'result', result])
-    documentsDone.push({ document, studentMainGroupName: studentMainGroupName, success: true })
-    await writeFile('data/documents-done.json', JSON.stringify(documentsDone, null, 2), 'utf-8')
-    await writeFile('data/documents-copy.json', JSON.stringify(documents, null, 2), 'utf-8')
-  } catch (error) {
-    logger('error', ['index', 'document', _id, 'student', student, 'teacher', teacher, 'studentMainGroupName', studentMainGroupName, 'result', error])
+  while (documents.lenght > 0) {
+    await sleep(1000)
+    logger('info', ['index', 'got', documents.length, 'documents left'])
+    const document = documents.pop()
+    const { _id, userId: teacher, studentUserName: student } = document
+    logger('info', ['index', 'document', _id, 'start'])
+    logger('info', ['index', 'document', _id, 'student', student, 'teacher', teacher, 'lookup student', 'start'])
+    const data = await getStudentData({ student, teacher })
+    logger('info', ['index', 'document', _id, 'student', student, 'teacher', teacher, 'lookup student', 'success'])
+    const { mainGroupName: studentMainGroupName } = data
+    logger('info', ['index', 'document', _id, 'student', student, 'teacher', teacher, 'studentMainGroupName', studentMainGroupName])
+    try {
+      const result = await updateDocument({ _id, studentMainGroupName })
+      logger('info', ['index', 'document', _id, 'student', student, 'teacher', teacher, 'studentMainGroupName', studentMainGroupName, 'result', result])
+      documentsDone.push({ document, studentMainGroupName: studentMainGroupName, success: true })
+      await writeFile('data/documents-done.json', JSON.stringify(documentsDone, null, 2), 'utf-8')
+      await writeFile('data/documents-copy.json', JSON.stringify(documents, null, 2), 'utf-8')
+    } catch (error) {
+      logger('error', ['index', 'document', _id, 'student', student, 'teacher', teacher, 'studentMainGroupName', studentMainGroupName, 'result', error])
+      process.exit(1)
+    }
   }
+  logger('info', ['index', 'finished'])
   process.exit()
 })()
